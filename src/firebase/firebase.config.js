57c855aae13f9ee31b1c6f03db5739a5014f7230
robiulXhasan <robi16275@gmail.com>
2023-05-02 06:49:54 +0600

    hide env file

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCXaWo3g_wEarBkDYjdCDkoY_EjYbuRRdQ",
-  authDomain: "the-news-dragon-a9068.firebaseapp.com",
-  projectId: "the-news-dragon-a9068",
-  storageBucket: "the-news-dragon-a9068.appspot.com",
-  messagingSenderId: "464253401187",
-  appId: "1:464253401187:web:3b37399b165b3952cb6056",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
